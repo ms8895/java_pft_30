636fccc725baae1f911f1a2608b822e67642a08d
Andrei Chernikov <ach910140@gmail.com>
2022-01-23 15:41:29 +0500

    Изменения после лекции 1.9

--- SandBox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ SandBox/src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -7,23 +7,15 @@ public class FirstProgram {
         hello("user");
         hello("world");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 6;
-        double b = 7;
-        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(6, 7);
+        System.out.println("Площадь прямоугольника со стороной " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello " + somebody + "!!!");
     }
 
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
-    }
 }
