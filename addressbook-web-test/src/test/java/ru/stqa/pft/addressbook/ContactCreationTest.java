1dd413fc5e9f4ba9b691b6d3fd139bc26796eaba
Andrei Chernikov <ach910140@gmail.com>
2022-01-31 16:33:27 +0500

    Занятие 2. Задание 4.
    Создан тест для  добавления контакта. Выполнена фикстура, рефакторинг. Выделены вспомогательные методы. Создан модульный класс ContactData.

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class GroupCreationTests {
+public class ContactCreationTest {
     private WebDriver wd;
     private JavascriptExecutor js;
 
@@ -24,49 +24,45 @@ public class GroupCreationTests {
 
     private void login(String username, String password) {
         wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
     @Test
-    public void testGroupCreation() throws Exception {
-        gotoGroupPage();
-        initGroupCreation();
-        fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
-        submitGroupCreation();
-        returnGroupPage();
+    public void testContactCreation() throws Exception {
+        gotoContactPage();
+        initContactPage();
+        fillContactForm(new ContactData("Ostap", "Bender", "+79998882211", "testTest@mail.ru"));
+        submit();
+        returnHomePage();
     }
 
-    private void returnGroupPage() {
-        wd.findElement(By.linkText("group page")).click();
+    private void returnHomePage() {
+        wd.findElement(By.linkText("home page")).click();
     }
 
-    private void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
+    private void submit() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
     }
 
-    private void initGroupCreation() {
-        wd.findElement(By.name("new")).click();
+    private void initContactPage() {
+        wd.findElement(By.name("firstname")).click();
     }
 
-    private void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+    private void gotoContactPage() {
+        wd.findElement(By.linkText("add new")).click();
     }
 
     @AfterMethod(alwaysRun = true)
