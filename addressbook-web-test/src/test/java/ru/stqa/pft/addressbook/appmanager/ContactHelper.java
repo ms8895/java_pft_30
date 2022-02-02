8388e1877e5db0aa8330f46a0b41ba079f7e6a86
Andrei Chernikov <ach910140@gmail.com>
2022-02-02 12:17:12 +0500

    Лекция 2.8 Коммит перед созданием HelperBase

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -20,13 +20,16 @@ public class ContactHelper {
     }
 
     public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+        type(By.name("firstname"), contactData.getFirstname());
+        type(By.name("lastname"), contactData.getLastname());
+        type(By.name("mobile"), contactData.getMobile());
+        type(By.name("email"), contactData.getEmail());
+    }
+
+    private void type(By locator, String text) {
+        wd.findElement(locator).click();
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
     }
 
     public void initContactCreation() {
