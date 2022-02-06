d894f4038aff35dd8ec9b55c7e147c7e01605c68
Andrei Chernikov <ach910140@gmail.com>
2022-02-06 19:58:08 +0500

    Лекция 3.3
    Добавлена возможность запуска тестов в разных браузерах: CHROME/FIREFOX/OPERA

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,13 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +18,26 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private JavascriptExecutor js;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-        wd = new ChromeDriver();
+        if (browser == BrowserType.CHROME) {
+            System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver\\chromedriver.exe");
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.FIREFOX) {
+            System.setProperty("webdriver.gecko.driver", "C:\\webdriver\\firefoxdriver\\geckodriver.exe");
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.OPERA) {
+            System.setProperty("webdriver.opera.driver", "C:\\webdriver\\operadriver\\operadriver.exe");
+
+            wd = new OperaDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         js = (JavascriptExecutor) wd;
         wd.get("http://localhost/addressbook/");
