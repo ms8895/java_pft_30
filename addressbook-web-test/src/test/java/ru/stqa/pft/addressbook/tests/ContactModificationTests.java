8927bce297e8d30d6e116c8dd5e40fc195853471
Andrei Chernikov <ach910140@gmail.com>
2022-02-16 10:42:06 +0500

    Задание 8. предусловия вынесено в метод

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 public class ContactModificationTests extends TestBase {
 
@@ -10,13 +9,7 @@ public class ContactModificationTests extends TestBase {
     public void testContactModification() {
         app.getNavigationHelper().gotoHomePage();
         if (!app.getContactHelper().isThereAContact()) {
-            app.getNavigationHelper().gotoGroupPage();
-            if (!app.getGroupHelper().isGroupPresent("Test1")) {
-                app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test"));
-            }
-            app.getNavigationHelper().gotoContactPage();
-            app.getContactHelper().createContact(new ContactData("Ostap", "Bender",
-                    "221B Baker Street", null, "testTest@mail.ru", "Test1"));
+            createGroupAndContact();
         }
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("Фрай", "Джей", null,
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class TestBase {
 
@@ -19,4 +21,13 @@ public class TestBase {
         app.stop();
     }
 
+    protected void createGroupAndContact() {
+        app.getNavigationHelper().gotoGroupPage();
+        if (!app.getGroupHelper().isGroupPresent("Test1")) {
+            app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test"));
+        }
+        app.getNavigationHelper().gotoContactPage();
+        app.getContactHelper().createContact(new ContactData("Ostap", "Bender",
+                "221B Baker Street", null, "testTest@mail.ru", "Test1"));
+    }
 }
