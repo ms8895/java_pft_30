5075617d282f0b96e9d7f9a41b1d67334f1e34bb
Andrei Chernikov <ach910140@gmail.com>
2022-02-17 11:26:33 +0500

    Лекция 4.5. Формирование коллекции
    В группах.

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,22 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
 
     @Test
     public void testGroupModification() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
         if (!app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("Test1", null, "Test3"));
         }
-        app.getGroupHelper().selectGroup(before-1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("Test2", "Test3", "Test1"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
