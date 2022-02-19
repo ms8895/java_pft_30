8ea940c57c8a935f5eb0aff3cea956b0849aa7fd
Andrei Chernikov <ach910140@gmail.com>
2022-02-19 08:55:54 +0500

    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -19,7 +19,7 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"Test2", "Test3", "Test1");
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "Test2", "Test3", "Test1");
         app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnGroupPage();
