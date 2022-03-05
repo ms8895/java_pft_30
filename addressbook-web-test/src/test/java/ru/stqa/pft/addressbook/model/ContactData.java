2a0524c0c1186fa0faafd0df711e36ef663e451d
Andrei Chernikov <ach910140@gmail.com>
2022-03-05 21:42:38 +0500

    Лекция 6.1. Пути к файлам и директориям

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.io.File;
 import java.util.Objects;
 
 public class ContactData {
@@ -17,6 +18,7 @@ public class ContactData {
     private String email3;
     private String allEmails;
     private String group;
+    private File photo;
 
     public int getId() {
         return id;
@@ -72,6 +74,10 @@ public class ContactData {
 
     public String getGroup() {
         return group;
+    }
+
+    public File getPhoto() {
+        return photo;
     }
 
     public ContactData withId(int id) {
@@ -142,6 +148,11 @@ public class ContactData {
 
     public ContactData withGroup(String group) {
         this.group = group;
+        return this;
+    }
+
+    public ContactData withPhoto(File photo) {
+        this.photo = photo;
         return this;
     }
 
--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -5,21 +5,26 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-    @Test(enabled = true)
+    @Test
     public void testContactCreation() throws Exception {
         app.goTo().homePage();
         Contacts before = app.сontact().all();
         GroupData group = new GroupData().withName("Test2");
-        ContactData contact = new ContactData().withFirstname("Ostap").withLastname("Bender").withAddress("221B Baker Street").withMobilePhone("+789456321").withEmail("testTest@mail.ru").withGroup(group.getName());
+        File photo = new File(".\\src\\test\\resources\\bu.jpg");
+        ContactData contact = new ContactData().withFirstname("Ostap").withLastname("Bender")
+                .withAddress("221B Baker Street").withPhoto(photo).withGroup(group.getName());
         app.сontact().createGroupAndContact(group, contact);
         assertThat(app.сontact().count(), equalTo(before.size() + 1));
         Contacts after = app.сontact().all();
         assertThat(after, equalTo(before.withAdded(contact
                 .withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
+
 }
