e3f7b956a120ee3bacd248a0109a0a7ad60f375b
Andrei Chernikov <ach910140@gmail.com>
2022-03-29 11:20:15 +0500

    Лекция 8.2. Подготовка проекта для тестирования MantisBT

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,64 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    private final Properties properties;
+    WebDriver wd;
+
+    private JavascriptExecutor js;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
+        if (browser.equals(BrowserType.CHROME)) {
+            System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver\\chromedriver.exe");
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty("webdriver.gecko.driver", "C:\\webdriver\\firefoxdriver\\geckodriver.exe");
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.OPERA)) {
+            System.setProperty("webdriver.opera.driver", "C:\\webdriver\\operadriver\\operadriver.exe");
+
+            wd = new OperaDriver();
+        }
+
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        js = (JavascriptExecutor) wd;
+        wd.get(properties.getProperty("web.baseUrl"));
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}
