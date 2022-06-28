c0af3d4528c7ae183f43a108f04476dbb1670a8e
Andrei Chernikov <ach910140@gmail.com>
2022-06-28 11:38:03 +0500

    Реализована возможность удаленного запуска браузера
    Лекция 10.2. Удалённый запуск браузера (Selenium Server)

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,17 +1,17 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -36,16 +36,21 @@ public class ApplicationManager {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
-
-        if (browser.equals(BrowserType.CHROME)) {
-            System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver\\chromedriver.exe");
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)) {
-            System.setProperty("webdriver.gecko.driver", "C:\\webdriver\\firefoxdriver\\geckodriver.exe");
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.OPERA)) {
-            System.setProperty("webdriver.opera.driver", "C:\\webdriver\\operadriver\\operadriver.exe");
-            wd = new OperaDriver();
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.CHROME)) {
+                System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver\\chromedriver.exe");
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.FIREFOX)) {
+                System.setProperty("webdriver.gecko.driver", "C:\\webdriver\\firefoxdriver\\geckodriver.exe");
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.OPERA)) {
+                System.setProperty("webdriver.opera.driver", "C:\\webdriver\\operadriver\\operadriver.exe");
+                wd = new OperaDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
